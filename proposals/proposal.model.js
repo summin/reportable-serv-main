a1e259343dcd17a12e511b8416c94c7ab4ff3172
Aleksandr Leontjev <alexander.leontjev@gmail.com>
2019-09-27 08:46:03 +0200

    dev

---
+++ proposals/proposal.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    ref: { type: String, unique: true, required: true },
+    supplierID: { type: String, required: true },
+    createdDate: { type: Date, default: Date.now }
+});
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('Proposal', schema);
